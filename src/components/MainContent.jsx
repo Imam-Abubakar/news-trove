753f54bee81de1761178f310dc427969cbc9bcb3
Imam Abubakar <imamabubakar2002@gmail.com>
2023-08-05 01:10:54 +0000

    (feature): Pagination

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,31 +1,19 @@
 import React, { useState, useEffect } from "react";
 import NewsCard from "./NewsCard";
+import newsData from "../assets/news.json";
 import axios from "axios";
+import ReactPaginate from "react-paginate";
+import "../assets/pagination.css";
 
 const MainContent = () => {
-  const [newsData, setNewsData] = useState(null);
-
-  // Replace this with the actual news data fetched from the backend
-  /*
-  const newsData = [
-    {
-      title: 'Be firm against military coups...',
-      link: 'https://dailypost.ng/2023/07/15/be-firm-against-military-coups-tinubu-tasks-african-leaders/',
-      preview: 'President Bola Ahmed Tinubu, on Saturday in Nairobi, Kenya, called on African leaders...',
-      date: '2023-08-04',
-      photo_link: 'https://dailypost.ng/wp-content/uploads/2023/07/Tinubu-400x240.jpg',
-      outlet: 'dailypostng',
-    },
-    // Add more news items here
-  ];
-  */
+  // const [newsData, setNewsData] = useState(null);
 
   const getNews = () => {
     const config = {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      };
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
 
     axios
       .get("https://news_scraper-1-v7112287.deta.app/news", config)
@@ -38,26 +26,88 @@ const MainContent = () => {
         console.log(error);
       });
   };
+  const [selectedToggle, setSelectedToggle] = useState("yesterday");
+  const [filteredNews, setFilteredNews] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const itemsPerPage = 18;
+
   useEffect(() => {
-    getNews();
-  }, []);
+    const filterDataForCurrentAndPreviousDay = () => {
+      const currentDate = new Date().toISOString().slice(0, 10);
+      const previousDate = new Date();
+      previousDate.setDate(previousDate.getDate() - 1);
+      const formattedPreviousDate = previousDate.toISOString().slice(0, 10);
+
+      const startIndex = currentPage * itemsPerPage;
+      const endIndex = startIndex + itemsPerPage;
+      const newsToShow =
+        selectedToggle === "today"
+          ? newsData.filter((item) => item.date === currentDate)
+          : newsData.filter((item) => item.date === formattedPreviousDate);
+
+      setFilteredNews(newsToShow.slice(startIndex, endIndex));
+    };
+
+    filterDataForCurrentAndPreviousDay();
+  }, [selectedToggle, currentPage]);
+
+  const handlePageChange = ({ selected }) => {
+    setCurrentPage(selected);
+  };
+
+  const handleToggleChange = (event) => {
+    setSelectedToggle(event.target.value);
+  };
 
   return (
     <main className="flex-grow container mx-auto py-8">
+      <div className="mb-4">
+        <label className="block mb-2 text-sm font-bold text-gray-800">
+          Toggle:
+        </label>
+        <div className="flex items-center">
+          <input
+            type="radio"
+            id="today"
+            name="toggle"
+            value="today"
+            checked={selectedToggle === "today"}
+            onChange={handleToggleChange}
+          />
+          <label htmlFor="today" className="ml-2 mr-4">
+            Today's News
+          </label>
+          <input
+            type="radio"
+            id="yesterday"
+            name="toggle"
+            value="yesterday"
+            checked={selectedToggle === "yesterday"}
+            onChange={handleToggleChange}
+          />
+          <label htmlFor="yesterday" className="ml-2">
+            Yesterday's News
+          </label>
+        </div>
+      </div>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-        {newsData === null ?
-        'No News at this time'
-        :
-        <>
-        {newsData?.map((item) => (
+        {filteredNews.map((item) => (
           <NewsCard key={item.link} data={item} />
         ))}
-        </>
-    }
-        
+      </div>
+      <div className="flex justify-center mt-4">
+        <ReactPaginate
+          previousLabel={"< Previous"}
+          nextLabel={"Next >"}
+          breakLabel={"..."}
+          pageCount={Math.ceil(filteredNews.length / itemsPerPage)}
+          pageRangeDisplayed={4}
+          onPageChange={handlePageChange}
+          containerClassName={"pagination"}
+          activeClassName={"active"}
+        />
       </div>
     </main>
   );
 };
-
 export default MainContent;
--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,27 +1,34 @@
-import React from 'react';
+import React from "react";
+import NoImage from "../assets/no-image.jpg";
 
 const NewsCard = ({ data }) => {
   const { title, link, preview, date, photo_link, outlet } = data;
 
   return (
-    <div className="bg-white rounded-lg shadow-md p-4">
+    <>
       {photo_link && (
-        <img src={photo_link} alt={title} className="w-full h-40 object-cover mb-4 rounded-lg" />
+        <div className="bg-white rounded-lg shadow-md p-4">
+          <img
+            src={photo_link === null ? NoImage : photo_link}
+            alt={title}
+            className="w-full h-40 object-cover mb-4 rounded-lg"
+          />
+          <h2 className="text-lg font-bold mb-2">{title}</h2>
+          <p className="text-sm text-gray-600 mb-4">{preview}</p>
+          <div className="flex justify-between items-center">
+            <span className="text-sm text-gray-500">{date}</span>
+            <a
+              href={link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 hover:underline"
+            >
+              Read more
+            </a>
+          </div>
+        </div>
       )}
-      <h2 className="text-lg font-bold mb-2">{title}</h2>
-      <p className="text-sm text-gray-600 mb-4">{preview}</p>
-      <div className="flex justify-between items-center">
-        <span className="text-sm text-gray-500">{date}</span>
-        <a
-          href={link}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-blue-500 hover:underline"
-        >
-          Read more
-        </a>
-      </div>
-    </div>
+    </>
   );
 };
 
